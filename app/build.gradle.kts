e2e9ddc99a1fc1f98ba1b1ada90526cd52118cfb
roz0075g <roz0075g@gmail.com>
2026-09-06 13:09:16 +0900

    Fix JVM toolchain for Android build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.roz0075g.timergame"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     compileOptions {
@@ -23,6 +23,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 
     buildFeatures {
