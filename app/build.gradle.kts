c86d77cf4aa134474d57df7406fa9cac4234ca2e
roz0075g <roz0075g@gmail.com>
2026-09-06 14:17:04 +0900

    Fix unit test Main dispatcher setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
